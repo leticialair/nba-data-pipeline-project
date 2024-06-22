261bae223e7ec5a1961ef094991200e27304e09c
Letícia <leticialair@hotmail.com>
2024-06-21 21:39:45 -0300

    feat: method pounds_to_kg

--- utils/treatments.py
+++ utils/treatments.py
@@ -7,7 +7,7 @@ class Treatments:
     Classe utilizada para inserir métodos de tratamento que serão atualizados em demais dataframes.
     """
 
-    def feet_to_meters(altura_ft: float) -> float:
+    def feet_to_meters(self, altura_ft: float) -> float:
         """
         Args:
             altura_ft: altura em pés.
@@ -17,6 +17,17 @@ class Treatments:
         """
 
         return altura_ft * 0.3048
+
+    def pounds_to_kg(self, peso_lbs: int) -> float:
+        """
+        Args:
+            peso_pounds: peso em libras.
+
+        Returns:
+            float: peso em quilos.
+        """
+
+        return peso_lbs * 0.4536
 
 
 treatments = Treatments()
